bdfd69c0bf9bf88aaf60e662a6e66a2575822d8e
DrAmanJy <dramanjy0770@gmail.com>
2026-05-06 16:37:57 +0530

    feat: integrate CORS support and enhance error handling in Express app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
+import { errorHandler } from "./middleware/error.middleware.js";
+import { AppError } from "./utils/AppError.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "1kb" }));
 
+app.use((req, res, next) =>
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 501))
+);
+
+app.use(errorHandler);
 export default app;
