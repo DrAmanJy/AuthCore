81b1e01ca3b0bf3efde0dce48d6834bc695e0d40
DrAmanJy <dramanjy0770@gmail.com>
2026-05-07 16:30:10 +0530

    feat: implement user login functionality with session management and access token generation

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -4,6 +4,8 @@ import Users from "../users/user.model.js";
 
 import { env } from "../../config/env.js";
 import { AppError } from "../../utils/AppError.js";
+import { createSession } from "../sessions/session.service.js";
+import { createAccessToken } from "../../utils/token.js";
 
 export const registerUser = async ({ name, email, password }) => {
   const existingUser = await Users.exists({ email });
@@ -20,5 +22,38 @@ export const registerUser = async ({ name, email, password }) => {
     password: hashedPassword,
   });
 
+  //todo send email with otp to verify account
+
   return user;
+};
+
+export const loginUser = async ({
+  email,
+  password,
+  device,
+  ipAddress,
+  userAgent,
+  serviceId,
+  serviceName,
+}) => {
+  const user = await Users.findOne({ email });
+
+  if (!user) throw new AppError("Invalid email or password", 400);
+  if (!user.isVerified) throw new AppError("Verify your account before login", 401);
+  if (!user.isActive) throw new AppError("Inactive account", 400);
+
+  const isValidPass = await bcrypt.compare(password, user.password);
+  if (!isValidPass) throw new AppError("Invalid email or password", 400);
+
+  const { plainToken: refreshToken, sessionId } = await createSession(
+    user._id,
+    serviceId,
+    device,
+    ipAddress,
+    userAgent
+  );
+
+  const accessToken = createAccessToken(user._id, sessionId, serviceName);
+
+  return { user, refreshToken, accessToken };
 };
