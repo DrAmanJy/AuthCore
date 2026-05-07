524ce9f54a3f5718bd8b2260e88b56da494893d7
DrAmanJy <dramanjy0770@gmail.com>
2026-05-07 13:31:46 +0530

    feat: add validation middleware for request body and ID format

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -1,6 +1,17 @@
+import { isValidObjectId } from "mongoose";
+import { AppError } from "../utils/AppError.js";
+
 export const validate =
   (schema, source = "body") =>
   (req, res, next) => {
     req[source] = schema.parse(req[source]);
+    next();
+  };
+
+export const validateId =
+  (source = "id") =>
+  (req, res, next) => {
+    const id = req.params[source];
+    if (!isValidObjectId(id)) throw new AppError("Invalid ID format", 400);
     next();
   };
