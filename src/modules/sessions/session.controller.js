bd6035541106045a322f627c9bdd5252fb2c02cb
DrAmanJy <dramanjy0770@gmail.com>
2026-05-07 13:23:18 +0530

    feat: add controller to retrieve all user sessions

--- src/modules/sessions/session.controller.js
+++ src/modules/sessions/session.controller.js
@@ -1,5 +1,5 @@
 import { AppError } from "../../utils/AppError.js";
-import { findSessionById } from "./session.service.js";
+import { findSessionById, findUserSessions } from "./session.service.js";
 
 export const getUserSessionById = async (req, res) => {
   const sessionId = req.params.sessionId;
@@ -11,5 +11,16 @@ export const getUserSessionById = async (req, res) => {
     status: "success",
     message: "Session successfully found",
     data: { session },
+  });
+};
+
+export const getUserSessions = async (req, res) => {
+  const { sub: userId } = req.accessToken;
+
+  const sessions = await findUserSessions(userId);
+  return res.status(200).json({
+    status: "success",
+    message: "Sessions successfully found",
+    data: { sessions },
   });
 };
