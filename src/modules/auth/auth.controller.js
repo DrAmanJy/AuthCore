bbed69e07b7d7ec2cb37e13e9bc95eb646314280
DrAmanJy <dramanjy0770@gmail.com>
2026-05-07 16:32:07 +0530

    feat: implement user login functionality with client info tracking and secure cookie handling

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,3 +1,5 @@
+import { env } from "../../config/env.js";
+import { getClientInfo } from "../../utils/clientInfo.utils.js";
 import * as authService from "./auth.service.js";
 
 export const register = async (req, res) => {
@@ -8,6 +10,36 @@ export const register = async (req, res) => {
     message: "User registered successfully",
     data: {
       user,
+    },
+  });
+};
+
+export const login = async (req, res) => {
+  const { ipAddress, userAgent, device } = getClientInfo(req);
+  const { _id: serviceId, name: serviceName } = req.service;
+
+  const { user, accessToken, refreshToken } = await authService.loginUser({
+    ...req.body,
+    ipAddress,
+    userAgent,
+    device,
+    serviceId,
+    serviceName,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
+    data: {
+      user,
+      accessToken,
     },
   });
 };
