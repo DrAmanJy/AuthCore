0a6a597180b3a7ce776b34f6ca027b910bd6227a
DrAmanJy <dramanjy0770@gmail.com>
2026-05-08 13:19:36 +0530

    feat: add error handling for missing 'x-target-audience' header in service identity middleware

--- src/middleware/serviceIdentity.middleware.js
+++ src/middleware/serviceIdentity.middleware.js
@@ -1,5 +1,11 @@
+import { AppError } from "../utils/AppError.js";
+
 export const extractServiceIdentity = (req, res, next) => {
   const targetAudience = req.headers["x-target-audience"];
+
+  if (!targetAudience) {
+    throw new AppError("Bad Request: Missing 'x-target-audience' header", 400);
+  }
 
   req.service = {
     name: targetAudience,
