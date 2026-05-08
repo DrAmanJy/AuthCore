4899bbccf84eab49d4408535dcb1faa6ed722934
DrAmanJy <dramanjy0770@gmail.com>
2026-05-08 13:56:03 +0530

    feat: implement email service for sending verification emails with OTP

---
+++ src/services/mail/email.service.js
@@ -0,0 +1,27 @@
+import { resend } from "../../config/resend.js";
+import { env } from "../../config/env.js";
+import { generateOtpEmailTemplate } from "./otpEmail.template.js";
+import { AppError } from "../../utils/AppError.js";
+
+export const sendVerificationEmail = async (userEmail, username, otpCode) => {
+  try {
+    const htmlContent = generateOtpEmailTemplate(otpCode, username);
+
+    const { data, error } = await resend.emails.send({
+      from: `PulseTrack <onboarding@${env.EMAIL_DOMAIN}>`,
+      to: userEmail,
+      subject: "Verify your PulseTrack account",
+      html: htmlContent,
+    });
+
+    if (error) {
+      console.error("Resend API Error:", error);
+      throw new AppError("Failed to send verification email", 500);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Email Service Error:", error);
+    throw new AppError("Internal server error while sending email", 500);
+  }
+};
