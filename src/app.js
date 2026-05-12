f22b08d8921db1745739baabb5813e6abc4292a6
DrAmanJy <dramanjy0770@gmail.com>
2026-05-12 23:14:55 +0530

    feat: configure swagger docs and trust proxy in express app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,56 @@
 import express from "express";
 import cors from "cors";
+
 import { errorHandler } from "./middleware/error.middleware.js";
 import { AppError } from "./utils/AppError.js";
 
-const app = express();
+import userRouter from "./modules/users/user.routes.js";
+import sessionRouter from "./modules/sessions/session.routes.js";
+import authRouter from "./modules/auth/auth.routes.js";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
-app.use(cors());
-app.use(express.json({ limit: "1kb" }));
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'AuthCore API',
+      version: '1.0.0',
+      description: 'Centralized authentication and session management service',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ['./src/modules/**/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+const app = express();
+app.set("trust proxy", 1);
+
+app.use(cors({ credentials: "included" }));
+
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/session", sessionRouter);
+
+app.use("/docs", ...swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req, res, next) =>
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 501))
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 );
 
 app.use(errorHandler);
