809e41f61fbdf28c9dda847b8bfe5c7798fabd2c
DrAmanJy <dramanjy0770@gmail.com>
2026-09-07 15:35:34 +0530

    feat(database): add user types definition (ts)

---
+++ packages/database/src/repositories/user.types.ts
@@ -0,0 +1,36 @@
+import type { UserStatus } from "../models/user.model.js";
+
+export type UserId = string;
+
+export type User = {
+  id: UserId;
+  email: string;
+  displayName: string;
+  status: UserStatus;
+  lastLoginAt?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type UserCredentials = {
+  id: UserId;
+  email: string;
+  passwordHash: string;
+};
+
+export type CreateUserData = {
+  email: string;
+  passwordHash: string;
+  displayName: string;
+  emailVerified?: boolean;
+  status?: UserStatus;
+  lastLoginAt?: Date;
+};
+
+export type UpdateUserData = {
+  email?: string;
+  displayName?: string;
+  emailVerified?: boolean;
+  status?: UserStatus;
+  lastLoginAt?: Date;
+};
