145e4844fa3aa5c3ad5363ff13369c5d59d4301f
DrAmanJy <dramanjy0770@gmail.com>
2026-09-21 14:54:49 +0530

    feat(contracts): add UserParams type

--- packages/contracts/src/users/types.ts
+++ packages/contracts/src/users/types.ts
@@ -5,9 +5,11 @@ import type { changeStatusSchema } from "./schemas/change-status.schema.js";
 import type { updateUserProfile } from "./schemas/update-profile.schema.js";
 import type { userProfileSchema } from "./schemas/user-profile.schema.js";
 import type { changePasswordSchema } from "./schemas/change-password.schema.js";
+import type { userParamsSchema } from "./schemas/user-params.schema.js";
 
 export type UserProfile = z.infer<typeof userProfileSchema>;
 export type UpdateProfile = z.infer<typeof updateUserProfile>;
 export type ChangeEmail = z.infer<typeof changeEmailSchema>;
 export type ChangeStatus = z.infer<typeof changeStatusSchema>;
 export type ChangePassword = z.infer<typeof changePasswordSchema>;
+export type UserParams = z.infer<typeof userParamsSchema>;
