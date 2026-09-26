ef4259dc6f1fceff69b262f2807fa3dc25a978f9
DrAmanJy <dramanjy0770@gmail.com>
2026-09-26 15:53:51 +0530

    feat(cookie-parser): add cookie-parser middleware and update dependencies

--- apps/auth-api/src/app/app.ts
+++ apps/auth-api/src/app/app.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import { config } from "@authcore/config";
 
 import appRouter from "./routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", appRouter);
 
